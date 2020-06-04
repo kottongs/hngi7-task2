371571a1a45cb5a4bb152131e68817e585ebcd7d
ishaku ibrahim <kottongs@yahoo.com>
2020-06-04 23:38:10 +0100

    Add files via upload

---
+++ ishakuscript.js
@@ -0,0 +1,7 @@
+const name = 'Ishaku Ibrahim';
+const HNG_id = 'HNG-04505';
+const email = 'babanpembi@gmail.com';
+const language = 'javascript';
+
+console.log('Hello World, this is '+ name +'with HNGi7 ID ' + HNG_id + ' using ' + language + 'for stage 2 task' + email);
+
