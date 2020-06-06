f203078bdcce612a7908b2f1217bdd72a5228144
ishaku ibrahim <kottongs@yahoo.com>
2020-06-06 22:52:53 +0100

    Add files via upload

---
+++ ishakuscript.py
@@ -0,0 +1,6 @@
+name = "Ishaku Ibrahim"
+hng_id = "HNG-04505"
+email = "babanpembi@gmail.com"
+language = "Python"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task " + "email " + email)
